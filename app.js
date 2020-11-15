bc010467af1c923e47b3f0b8c4e01ac9b8485e8d
blink-maker4 <bautista.sarahmae19@gmail.com>
2020-11-15 21:35:51 +0800

    adds new configuration

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
 
 const sequelize = require('./msql_connection');
 
 //get todo route and use
 const todoRoutes = require('./routes/todoRoutes');
 app.use('/todos', todoRoutes);
+
+app.post('/test', (req, res)=> {
+    res.json({data: req.body});
+});
 
 //set up server 
 app.listen(PORT, () => console.log(`Server is listening in port ${PORT}`));
